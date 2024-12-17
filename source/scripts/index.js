b024f47a12fa8e21ffee2ab6b1b672307af5038c
MadinaSatem <151987064+MadinaSatem@users.noreply.github.com>
2024-12-17 15:41:17 +0300

    проблемы в javascript

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
-/* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.navigation');
+const navToggle = document.querySelector('.navigation__menu--toggle');
+
+navMain.classList.remove('navigation__menu--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('navigation--closed')) {
+    navMain.classList.remove('navigation--closed');
+    navMain.classList.add('navigation--opened');
+  } else {
+    navMain.classList.remove('navigation--opened');
+    navMain.classList.add('navigation--closed');
+  }
+});
+console.log('Привет мир!');
